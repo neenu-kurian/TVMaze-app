4564d2d9fe0775ac7c6efac35edfa817cff5913e
neenukurian92@gmail.com <neenukurian92@gmail.com>
2022-06-07 14:13:15 +0200

    added rating fix

--- src/common/ShowCard.vue
+++ src/common/ShowCard.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="show__container">
     <div class="show__wrapper">
-      <div class="show__tile" v-for="({name,image,id,genres},index) in shows" :key="index">
+      <div class="show__tile" v-for="({name,image,id,genres,rating},index) in shows" :key="index">
         <router-link class="show__link" :to="`/show/${id}`">
           <img class="show__img" v-if="image!==null" :src="image.original" alt="show" />
           <img class="show__img" v-else src="../assets/images/default.jpeg" alt="fallback" />
-          <div>
+          <div class="show__details">
           <h2 class="show__title">{{name}}</h2>
           <span class="show__genre" v-if="genres.length">{{genres[0]}}</span>
+          <img src="../assets/images/star-regular.png" alt="rating"  v-if="rating.average" class="show__rating--icon"/>
+          <span v-if="rating.average" class="show__rating">{{rating.average}}</span>
           </div>
         </router-link>
       </div>
