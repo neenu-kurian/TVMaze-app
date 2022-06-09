ccc56b395d5aca321cbc6cc84dc9fcffe6ee03bd
neenukurian92@gmail.com <neenukurian92@gmail.com>
2022-06-09 12:49:47 +0200

    added unit test

--- tests/unit/homepage.spec.js
+++ tests/unit/homepage.spec.js
@@ -20,6 +20,7 @@ describe('HomePage', () => {
     getters = {
       shows: () => [
         {
+          name: 'Hells Kitchen',
           image: {
             original: 'https://static.tvmaze.com/uploads/images/original_untouched/1/4601.jpg'
           },
@@ -45,8 +46,62 @@ describe('HomePage', () => {
         RouterLink: RouterLinkStub
       }
     })
-
     wrapper.find('button').trigger('submit')
     expect(mutations.SET_SEARCH.mock.calls).toHaveLength(1)
+  })
+
+  it('renders a values from getters', () => {
+    const wrapper = mount(HomePage, {
+      store,
+      localVue,
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    })
+    expect(wrapper.find('.show__rating')
+      .text().trim()).toEqual('8')
+    expect(wrapper.find('.show__title')
+      .text().trim()).toEqual('Hells Kitchen')
+    expect(wrapper.find('.show__genre')
+      .text().trim()).toEqual('comedy')
+  })
+
+  it('renders loading bar only on loading', () => {
+    const wrapper = mount(HomePage, {
+      computed: {
+        loading: () => true
+      },
+      store,
+      localVue,
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    })
+    expect(wrapper.find('.loader').exists()).toBe(true)
+  })
+
+  it('doesnt render loading bar when loading is finished', () => {
+    const wrapper = mount(HomePage, {
+      computed: {
+        loading: () => false
+      },
+      store,
+      localVue,
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    })
+    expect(wrapper.find('.loader').exists()).toBe(false)
+  })
+
+  it('render shows only if it is not empty', () => {
+    const wrapper = mount(HomePage, {
+      store,
+      localVue,
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    })
+    expect(wrapper.find('.show__container').exists()).toBe(true)
   })
 })
